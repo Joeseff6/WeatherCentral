8cb64e25ec6a9f88eb9217dfc7e3e2ef8b182c8e
Joseph Soria <josephsoria6@gmail.com>
2020-12-27 17:38:28 -0600

    Assigned variable to form and button. Add function to button

---
+++ Assets/script.js
@@ -0,0 +1,8 @@
+const searchForm = $(`#searchForm`)
+const searchBtn = $(`#locationSearch`)
+console.log(searchField)
+
+searchBtn.click(function(event) {
+    event.preventDefault()
+    console.log(`hey`)
+})
