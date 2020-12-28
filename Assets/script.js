377ad959225218d3822201be541d911128f54400
Joseph Soria <josephsoria6@gmail.com>
2020-12-27 20:42:51 -0600

    Added click/submit functions when search button is clicked on

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,29 @@
 const searchForm = $(`#searchForm`);
 const searchBtn = $(`#searchBtn`);
+const searchVal = $(`#searchLocation`)
+
 var cityList = [];
+var weatherAPIKey = `e1e7a51f8e49f8c421e774a281377b83`;
 
 
 
+searchBtn.click(function() {
+    searchForm.submit(function(event) {
+        event.preventDefault();
+        var city = searchVal.val();
+        $(`#city`).text(`City Name: ${city}`)
+        var weatherURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${weatherAPIKey}`;
+        
+        getResponse()
 
-
-searchBtn.click(function(event) {
-    event.preventDefault();
+        function getResponse() {
+            $.ajax({
+                url: weatherURL,
+                method: `GET`
+            })
+            .then(function(response) {
+            })
+        }
+    })
 })
+
