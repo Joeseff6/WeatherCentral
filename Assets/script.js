c6ea21932e10d4dd7e39ee49c8a058165297cc84
Joseph Soria <josephsoria6@gmail.com>
2020-12-29 00:59:32 -0600

    Made variables for forecasting

--- Assets/script.js
+++ Assets/script.js
@@ -12,34 +12,66 @@ searchBtn.click(function() {
         event.preventDefault();
         var city = searchVal.val();
         $(`#city`).text(`City Name: ${city}`)
+
+
         var forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${weatherAPIKey}`;
-        var locationURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherAPIKey}`
-        
-        $.ajax({
-            url: locationURL,
-            method: `GET`
-        })
-            .then (function(response) {
-                var lon = response.coord.lon;
-                var lat = response.coord.lat
-                var lonlatArr = [lon,lat]
-                return lonlatArr
-            })
 
-            // console.log(lon)
-            // console.log(lat)
-            console.log(lonlatArr)
-
-
-        // var uviURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${weatherAPIKey}`
-        
-        getResponse()
-
-        function getResponse() {
             $.ajax({
                 url: forecastURL,
                 method: `GET`
             })
+
+                .then(function(response) {
+                    let date = response.list[0].dt_txt;
+                    let icon = Object.values(response.list[0].weather[0])[3];
+                    let tempF = ((Object.values(response.list[0].main)[0]-273.15)*1.8+32).toFixed(2);
+                    let humidity = Object.values(response.list[0].main)[7]
+                    let windspeed = Object.values(response.list[0].wind)[0]
+
+                    console.log(response)
+
+
+
+
+                })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // var locationURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherAPIKey}`
+        
+        // $.ajax({
+        //     url: locationURL,
+        //     method: `GET`
+        // })
+
+    
+
+
+        // var uviURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${weatherAPIKey}`
+        
+
+
             
 
 
@@ -48,25 +80,8 @@ searchBtn.click(function() {
             //     method: `GET`
             // })
 
-            .then(function(response) {
-                let date = response.list[0].dt_txt;
-                let icon = Object.values(response.list[0].weather[0])[3]
-                let tempF = (parseInt(Object.values(response.list[0].main[0])[0])-273.15)*1.8+32;
-                let humidity = Object.values(response.list[0].main[0])[7]
-                let windspeed = Object.values(response.list[0].wind[0])[0]
 
-                console.log(response)
-                console.log(date)
-                console.log(icon)
-                console.log(tempF)
-                console.log(humidity)
-                console.log(windspeed)
-
-
-
-            })
-        }
-    })
+        })
 })
 
 // function getInfo() {
