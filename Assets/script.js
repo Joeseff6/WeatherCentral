6e1b6e0252d3032761452c46a67e43db70b45a30
Joseph Soria <josephsoria6@gmail.com>
2020-12-29 00:47:10 -0600

    Working on APIs

--- Assets/script.js
+++ Assets/script.js
@@ -12,19 +12,68 @@ searchBtn.click(function() {
         event.preventDefault();
         var city = searchVal.val();
         $(`#city`).text(`City Name: ${city}`)
-        var weatherURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${weatherAPIKey}`;
+        var forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${weatherAPIKey}`;
+        var locationURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherAPIKey}`
+        
+        $.ajax({
+            url: locationURL,
+            method: `GET`
+        })
+            .then (function(response) {
+                var lon = response.coord.lon;
+                var lat = response.coord.lat
+                var lonlatArr = [lon,lat]
+                return lonlatArr
+            })
+
+            // console.log(lon)
+            // console.log(lat)
+            console.log(lonlatArr)
+
+
+        // var uviURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${weatherAPIKey}`
         
         getResponse()
 
         function getResponse() {
             $.ajax({
-                url: weatherURL,
+                url: forecastURL,
                 method: `GET`
             })
+            
+
+
+            // $.ajax({
+            //     url: uviURL,
+            //     method: `GET`
+            // })
+
             .then(function(response) {
+                let date = response.list[0].dt_txt;
+                let icon = Object.values(response.list[0].weather[0])[3]
+                let tempF = (parseInt(Object.values(response.list[0].main[0])[0])-273.15)*1.8+32;
+                let humidity = Object.values(response.list[0].main[0])[7]
+                let windspeed = Object.values(response.list[0].wind[0])[0]
+
+                console.log(response)
+                console.log(date)
+                console.log(icon)
+                console.log(tempF)
+                console.log(humidity)
+                console.log(windspeed)
+
+
 
             })
         }
     })
 })
 
+// function getInfo() {
+//     let hourIterator = 0;
+//     let weatherArr = [];
+//     let mainArr = [];
+//     for (let i = 0; i < 16; i++) {
+//         let icon = response.list.weather.icon
+//     }
+// }
