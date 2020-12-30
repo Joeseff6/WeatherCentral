c7de66b3fbaf9ea19a54762e0e58236104ea3367
Joseph Soria <josephsoria6@gmail.com>
2020-12-30 17:48:05 -0600

    Finished script for UVI

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,12 @@
 const searchForm = $(`#searchForm`);
 const searchBtn = $(`#searchBtn`);
 const searchVal = $(`#searchLocation`)
-
 var cityList = [];
 var weatherAPIKey = `e1e7a51f8e49f8c421e774a281377b83`;
 
-
-
 searchBtn.click(function() {
     var city = searchVal.val();
+    $(`.forecastSection`).removeClass(`d-none`)
     $(`<button>${city}</button>`).appendTo(`#searchHistory`)
     searchForm.submit(function(event) {
         event.preventDefault();
@@ -19,20 +17,13 @@ searchBtn.click(function() {
         $(`#dayHeaderText4`).text(`Day Four`)
         $(`#dayHeaderText5`).text(`Day Five`)
 
-
-
-
-
         var forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${weatherAPIKey}`;
 
             $.ajax({
                 url: forecastURL,
                 method: `GET`
             })
-
                 .then(function(response) {
-                    console.log(forecastURL)
-
                     let hourIterator = 0;
                     let date = ``;
                     let icon = ``;
@@ -40,86 +31,67 @@ searchBtn.click(function() {
                     let humidity = 0;
                     let windspeed = 0;
                     let counter = 0;
-
                     for (let i = 1; i < 16; i++) {
                         icon = Object.values(response.list[hourIterator].weather[0])[3];
                         date = response.list[hourIterator].dt_txt;
                         tempF = (Object.values(response.list[hourIterator].main)[0]).toFixed(2);
                         humidity = Object.values(response.list[hourIterator].main)[7];
                         windspeed = Object.values(response.list[hourIterator].wind)[0];
-
                         $(`#icon${i}`).attr(`src`,`https://openweathermap.org/img/wn/${icon}@2x.png`);
                         $(`#date${i}`).text(`Day: ${date}`);
                         $(`#temp${i}`).text(`Temp: ${tempF}\u00B0F`);
                         $(`#humidity${i}`).text(`Humidity: ${humidity}%`);
                         $(`#windSpeed${i}`).text(`Wind Speed: ${windspeed}mph`);
-
-                    
                         hourIterator += 2
                         counter++
                         if (counter === 3 && hourIterator < 36) {
                             counter = 0
                             hourIterator += 2
                         }
+                    }
 
+                    var lat = Object.values(response.city.coord)[0];
+                    var lon = Object.values(response.city.coord)[1];
+                    console.log(lat)
+                    console.log(lon)
 
+                    var uviURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${weatherAPIKey}`
 
-                }
+                    $.ajax({
+                        url: uviURL,
+                        method: `GET`
+                    })
+                        .then(function(response) {
+                            let uvi = 0
+                            console.log(response)
 
+                            for (let i = 0; i < 5; i++) {
+                                uvi = Math.round(Object.values(response.daily[i])[14])
+                                uviArrLength = (Object.values(response.daily[i])).length
+                                console.log(uviArrLength)
 
-                        console.log(response)
+                                if (uviArrLength === 14) {
+                                    uvi = Math.round(Object.values(response.daily[i])[13])
+                                }
 
+                                if (uvi <= 2) {
+                                    var uviCondition = `Low`
+                                } else if (uvi <= 5) {
+                                    var uviCondition = `Moderate`
+                                } else if (uvi <= 7) {
+                                    var uviCondition = `High`
+                                } else if (uvi <= 10) {
+                                    var uviCondition = `Very High`
+                                } else {
+                                    var uviCondition = `Extreme`
+                                }
+    
+                                $(`#uviText${i}`).text(`UV Index: ${uvi}`);
+                                $(`#uviCondition${i}`).text(uviCondition);
 
-
-
+                            }
+                        })
                 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-        // var uviURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${weatherAPIKey}`
-        
-
-
-            
-
-
-            // $.ajax({
-            //     url: uviURL,
-            //     method: `GET`
-            // })
-
-
-        })
+    })
 })
-
-// function getInfo() {
-//     let hourIterator = 0;
-//     let weatherArr = [];
-//     let mainArr = [];
-//     for (let i = 0; i < 16; i++) {
-//         let icon = response.list.weather.icon
-//     }
-// }
